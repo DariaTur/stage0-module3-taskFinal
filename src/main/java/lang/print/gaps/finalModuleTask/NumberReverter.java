17298387f0087340604c659234d21c7f353f8291
Daria <dashatur100500@gmail.com>
2022-06-15 23:05:16 +0300

    edit number reverter

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -4,9 +4,10 @@ public class NumberReverter {
     public void revert(int number) {
         String res = "";
         String num = ""+number;
-        for(int i = num.length()-1; i>0;i--){
+        for(int i = num.length()-1; i>=0;i--){
             res+= Character.toString(num.charAt(i));
         }
         System.out.println(Integer.parseInt(res));
     }
+
 }
