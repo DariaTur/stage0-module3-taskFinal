aa2c605b70049efbbe33f33d11564d5309854fbf
Daria <dashatur100500@gmail.com>
2022-06-15 22:57:37 +0300

    add mod 3 final task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        String num = ""+number;
+        for(int i = 0; i<num.length();i++){
+            sum+= Integer.parseInt(Character.toString(num.charAt(i)));
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String res = "";
+        String num = ""+number;
+        for(int i = num.length()-1; i>0;i--){
+            res+= Character.toString(num.charAt(i));
+        }
+        System.out.println(Integer.parseInt(res));
     }
 }
